509598d90bd08fdaf16fb28fa1a8c7fd5bb9363c
GulomovCreative <gulomov@live.ru>
2018-10-03 01:02:52 +0700

    fix: Добавлены отсутсвующие описания настроек

--- core/components/modsizecontrol/lexicon/ru/setting.inc.php
+++ core/components/modsizecontrol/lexicon/ru/setting.inc.php
@@ -4,10 +4,10 @@ $_lang['area_modsizecontrol_main'] = 'Основные';
 //TODO: довести до ума
 
 $_lang['setting_modsizecontrol_site_limit'] = 'Лимит сайта';
-$_lang['setting_modsizecontrol_site_limit_desc'] = 'Описание будет позже';
+$_lang['setting_modsizecontrol_site_limit_desc'] = 'Укажите лимит сайта в байтах. По умолчанию: 1073741824, что раняется 1 Гб';
 
 $_lang['setting_modsizecontrol_site_size'] = 'Размер сайта';
-$_lang['setting_modsizecontrol_site_size_desc'] = 'Описание будет позже';
+$_lang['setting_modsizecontrol_site_size_desc'] = 'Здесь храниться размер сайта';
 
 $_lang['setting_modsizecontrol_file_system'] = 'Файловые системы';
 $_lang['setting_modsizecontrol_file_system_desc'] = 'Введите список ID файловых систем через запятую';
