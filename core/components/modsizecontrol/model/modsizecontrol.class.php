0b7df9f47db1da1dc47b3f67c6cbebd0b06c1b1e
Pavel Zarubin <pavel@orendat.ru>
2018-10-05 14:54:41 +0300

    #16 - Fix

--- core/components/modsizecontrol/model/modsizecontrol.class.php
+++ core/components/modsizecontrol/model/modsizecontrol.class.php
@@ -61,23 +61,41 @@ class modSizeControl
         return $ret;
     }
 
-    public function dir_size($dirName)
+    public function dir_size($dir)
     {
-        $totalSize = 0;
-        if ($dirStream = @opendir($dirName)) {
-            while (false !== ($filename = readdir($dirStream))) {
-                if ($filename != "." && $filename != "..") {
-                    if (is_file($dirName . "/" . $filename))
-                        $totalSize += filesize($dirName . "/" . $filename);
+        $dir = rtrim(str_replace('\\', '/', $dir), '/');
 
-                    if (is_dir($dirName . "/" . $filename))
-                        $totalSize += $this->dir_size($dirName . "/" . $filename);
+        if (is_dir($dir) === true) {
+            $totalSize = 0;
+            $os        = strtoupper(substr(PHP_OS, 0, 3));
+            // If on a Unix Host (Linux, Mac OS)
+            if ($os !== 'WIN') {
+                $io = popen('/usr/bin/du -sb ' . $dir, 'r');
+                if ($io !== false) {
+                    $totalSize = intval(fgets($io, 80));
+                    pclose($io);
+                    return $totalSize;
                 }
             }
+            // If on a Windows Host (WIN32, WINNT, Windows)
+            if ($os === 'WIN' && extension_loaded('com_dotnet')) {
+                $obj = new \COM('scripting.filesystemobject');
+                if (is_object($obj)) {
+                    $ref       = $obj->getfolder($dir);
+                    $totalSize = $ref->size;
+                    $obj       = null;
+                    return $totalSize;
+                }
+            }
+            // If System calls did't work, use slower PHP 5
+            $files = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir));
+            foreach ($files as $file) {
+                $totalSize += $file->getSize();
+            }
+            return $totalSize;
+        } else if (is_file($dir) === true) {
+            return filesize($dir);
         }
-
-        closedir($dirStream);
-        return $totalSize;
     }
 
 }
