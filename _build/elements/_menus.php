7943b951dd6a294df4b44f2e13f0f775a9d94b78
GulomovCreative <gulomov@live.ru>
2018-12-20 20:13:38 +0700

    Removed unecessary elements

--- _build/elements/_menus.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'modsizecontrol' => [
-        'description' => 'modsizecontrol_menu_desc',
-        'action' => 'home',
-        //'icon' => '<i class="icon icon-large icon-modx"></i>',
-    ],
-];
